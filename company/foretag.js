ffd417b92e59ad2d447d4443966d8b0836f27389
brillin <45040324+brillin@users.noreply.github.com>
2023-02-01 01:44:43 +0100

    Add files via upload

---
+++ company/foretag.js
@@ -0,0 +1,277 @@
+function displayComparisonLeft(event) {
+  document.getElementById("lct1").style.display = "none";
+  document.getElementById("lct2x").style.display = "none";
+  document.getElementById("lct5").style.display = "none";
+  document.getElementById("lprodigy0").style.display = "none";
+  document.getElementById("lprodigy0x").style.display = "none";
+  document.getElementById("lprodigy4").style.display = "none";
+  document.getElementById("ldictator2").style.display = "none";
+  document.getElementById("lagent1").style.display = "none";
+  document.getElementById("lagent4").style.display = "none";
+  document.getElementById("lblaster2").style.display = "none";
+  document.getElementById("ltrooper2vi").style.display = "none";
+  document.getElementById("lascender").style.display = "none";
+  document.getElementById("lgrimnir2vi").style.display = "none";
+  document.getElementById("lswitcher").style.display = "none";
+  document.getElementById("lagentpoleblack").style.display = "none";
+  document.getElementById("lfactionpolered").style.display = "none";
+  document.getElementById("lfactionpoleyellow").style.display = "none";
+  document.getElementById("lctpoleorange").style.display = "none";
+  document.getElementById("lctpoleblack").style.display = "none";
+  document.getElementById("lprodigypoleblue").style.display = "none";
+  document.getElementById("lprodigyjr").style.display = "none";
+  let value = event.value;
+  if (value == "ct1") {
+    document.getElementById("lct1").style.display = "block";
+  } else if (value == "ct2x") {
+    document.getElementById("lct2x").style.display = "block";
+  } else if (value == "ct5") {
+    document.getElementById("lct5").style.display = "block";
+  } else if (value == "prodigy0") {
+    document.getElementById("lprodigy0").style.display = "block";
+  } else if (value == "prodigy0x") {
+    document.getElementById("lprodigy0x").style.display = "block";
+  } else if (value == "prodigy4") {
+    document.getElementById("lprodigy4").style.display = "block";
+  } else if (value == "dictator2") {
+    document.getElementById("ldictator2").style.display = "block";
+  } else if (value == "agent1") {
+    document.getElementById("lagent1").style.display = "block";
+  } else if (value == "agent4") {
+    document.getElementById("lagent4").style.display = "block";
+  } else if (value == "blaster2") {
+    document.getElementById("lblaster2").style.display = "block";
+  } else if (value == "trooper2vi") {
+    document.getElementById("ltrooper2vi").style.display = "block";
+  } else if (value == "ascender") {
+    document.getElementById("lascender").style.display = "block";
+  } else if (value == "grimnir2vi") {
+    document.getElementById("lgrimnir2vi").style.display = "block";
+  } else if (value == "switcher") {
+    document.getElementById("lswitcher").style.display = "block";
+  } else if (value == "agentpoleblack") {
+    document.getElementById("lagentpoleblack").style.display = "block";
+  } else if (value == "factionpolered") {
+    document.getElementById("lfactionpolered").style.display = "block";
+  } else if (value == "factionpoleyellow") {
+    document.getElementById("lfactionpoleyellow").style.display = "block";
+  } else if (value == "ctpoleorange") {
+    document.getElementById("lctpoleorange").style.display = "block";
+  } else if (value == "ctpoleblack") {
+    document.getElementById("lctpoleblack").style.display = "block";
+  } else if (value == "prodigypoleblue") {
+    document.getElementById("lprodigypoleblue").style.display = "block";
+  } else if (value == "prodigyjr") {
+    document.getElementById("lprodigyjr").style.display = "block";
+  }
+}
+function displayComparisonRight(event) {
+  document.getElementById("rct1").style.display = "none";
+  document.getElementById("rct2x").style.display = "none";
+  document.getElementById("rct5").style.display = "none";
+  document.getElementById("rprodigy0").style.display = "none";
+  document.getElementById("rprodigy0x").style.display = "none";
+  document.getElementById("rprodigy4").style.display = "none";
+  document.getElementById("rdictator2").style.display = "none";
+  document.getElementById("ragent1").style.display = "none";
+  document.getElementById("ragent4").style.display = "none";
+  document.getElementById("rblaster2").style.display = "none";
+  document.getElementById("rtrooper2vi").style.display = "none";
+  document.getElementById("rascender").style.display = "none";
+  document.getElementById("rgrimnir2vi").style.display = "none";
+  document.getElementById("rswitcher").style.display = "none";
+  document.getElementById("ragentpoleblack").style.display = "none";
+  document.getElementById("rfactionpolered").style.display = "none";
+  document.getElementById("rfactionpoleyellow").style.display = "none";
+  document.getElementById("rctpoleorange").style.display = "none";
+  document.getElementById("rctpoleblack").style.display = "none";
+  document.getElementById("rprodigypoleblue").style.display = "none";
+  document.getElementById("rprodigyjr").style.display = "none";
+  let value = event.value;
+  if (value == "ct1") {
+    document.getElementById("rct1").style.display = "block";
+  } else if (value == "ct2x") {
+    document.getElementById("rct2x").style.display = "block";
+  } else if (value == "ct5") {
+    document.getElementById("rct5").style.display = "block";
+  } else if (value == "prodigy0") {
+    document.getElementById("rprodigy0").style.display = "block";
+  } else if (value == "prodigy0x") {
+    document.getElementById("rprodigy0x").style.display = "block";
+  } else if (value == "prodigy4") {
+    document.getElementById("rprodigy4").style.display = "block";
+  } else if (value == "dictator2") {
+    document.getElementById("rdictator2").style.display = "block";
+  } else if (value == "agent1") {
+    document.getElementById("ragent1").style.display = "block";
+  } else if (value == "agent4") {
+    document.getElementById("ragent4").style.display = "block";
+  } else if (value == "blaster2") {
+    document.getElementById("rblaster2").style.display = "block";
+  } else if (value == "trooper2vi") {
+    document.getElementById("rtrooper2vi").style.display = "block";
+  } else if (value == "ascender") {
+    document.getElementById("rascender").style.display = "block";
+  } else if (value == "grimnir2vi") {
+    document.getElementById("rgrimnir2vi").style.display = "block";
+  } else if (value == "switcher") {
+    document.getElementById("rswitcher").style.display = "block";
+  } else if (value == "agentpoleblack") {
+    document.getElementById("ragentpoleblack").style.display = "block";
+  } else if (value == "factionpolered") {
+    document.getElementById("rfactionpolered").style.display = "block";
+  } else if (value == "factionpoleyellow") {
+    document.getElementById("rfactionpoleyellow").style.display = "block";
+  } else if (value == "ctpoleorange") {
+    document.getElementById("rctpoleorange").style.display = "block";
+  } else if (value == "ctpoleblack") {
+    document.getElementById("rctpoleblack").style.display = "block";
+  } else if (value == "prodigypoleblue") {
+    document.getElementById("rprodigypoleblue").style.display = "block";
+  } else if (value == "prodigyjr") {
+    document.getElementById("rprodigyjr").style.display = "block";
+  }
+}
+let a1Showing = false;
+
+document.getElementById("q1").addEventListener("click", () => {
+  if (a1Showing == true) {
+    document.getElementById("a1").style.display = "none";
+    a1Showing = false;
+  } else if (a1Showing == false) {
+    document.getElementById("a1").style.display = "block";
+    a1Showing = true;
+  }
+});
+
+let a2Showing = false;
+
+document.getElementById("q2").addEventListener("click", () => {
+  if (a2Showing == true) {
+    document.getElementById("a2").style.display = "none";
+    a2Showing = false;
+  } else if (a2Showing == false) {
+    document.getElementById("a2").style.display = "block";
+    a2Showing = true;
+  }
+});
+
+let a3Showing = false;
+
+document.getElementById("q3").addEventListener("click", () => {
+  if (a3Showing == true) {
+    document.getElementById("a3").style.display = "none";
+    a3Showing = false;
+  } else if (a3Showing == false) {
+    document.getElementById("a3").style.display = "block";
+    a3Showing = true;
+  }
+});
+
+let a4Showing = false;
+
+document.getElementById("q4").addEventListener("click", () => {
+  if (a4Showing == true) {
+    document.getElementById("a4").style.display = "none";
+    a4Showing = false;
+  } else if (a4Showing == false) {
+    document.getElementById("a4").style.display = "block";
+    a4Showing = true;
+  }
+});
+
+function displayProduct(event) {
+  document.getElementById("ct1").style.display = "none";
+  document.getElementById("ct2x").style.display = "none";
+  document.getElementById("ct5").style.display = "none";
+  document.getElementById("prodigy0").style.display = "none";
+  document.getElementById("prodigy0x").style.display = "none";
+  document.getElementById("prodigy4").style.display = "none";
+  document.getElementById("dictator2").style.display = "none";
+  document.getElementById("agent1").style.display = "none";
+  document.getElementById("agent4").style.display = "none";
+  document.getElementById("helmet").style.display = "none";
+  document.getElementById("agentpoleblack").style.display = "none";
+  document.getElementById("poledefault").style.display = "none";
+  document.getElementById("prodigyjr").style.display = "none";
+  let value = event.value;
+  if (value == "ct1") {
+    document.getElementById("ct1").style.display = "block";
+  } else if (value == "ct2x") {
+    document.getElementById("ct2x").style.display = "block";
+  } else if (value == "ct5") {
+    document.getElementById("ct5").style.display = "block";
+  } else if (value == "prodigy0") {
+    document.getElementById("prodigy0").style.display = "block";
+  } else if (value == "prodigy0x") {
+    document.getElementById("prodigy0x").style.display = "block";
+  } else if (value == "prodigy4") {
+    document.getElementById("prodigy4").style.display = "block";
+  } else if (value == "dictator2") {
+    document.getElementById("dictator2").style.display = "block";
+  } else if (value == "agent1") {
+    document.getElementById("agent1").style.display = "block";
+  } else if (value == "agent4") {
+    document.getElementById("agent4").style.display = "block";
+  } else if (value == "blaster2") {
+    document.getElementById("helmet").style.display = "block";
+  } else if (value == "trooper2vi") {
+    document.getElementById("helmet").style.display = "block";
+  } else if (value == "ascender") {
+    document.getElementById("helmet").style.display = "block";
+  } else if (value == "grimnir2vi") {
+    document.getElementById("helmet").style.display = "block";
+  } else if (value == "switcher") {
+    document.getElementById("helmet").style.display = "block";
+  } else if (value == "trooper2vi") {
+    document.getElementById("helmet").style.display = "block";
+  } else if (value == "agentpoleblack") {
+    document.getElementById("agentpoleblack").style.display = "block";
+  } else if (value == "factionpolered") {
+    document.getElementById("poledefault").style.display = "block";
+  } else if (value == "factionpoleyellow") {
+    document.getElementById("poledefault").style.display = "block";
+  } else if (value == "ctpoleorange") {
+    document.getElementById("poledefault").style.display = "block";
+  } else if (value == "ctpoleblack") {
+    document.getElementById("poledefault").style.display = "block";
+  } else if (value == "prodigypoleblue") {
+    document.getElementById("poledefault").style.display = "block";
+  } else if (value == "prodigyjr") {
+    document.getElementById("prodigyjr").style.display = "block";
+  }
+}
+function enterRegion(event) {
+  document.getElementById("enterregion").style.display = "none";
+  let value = event.value;
+  if (value == "other") {
+    document.getElementById("enterregion").style.display = "block";
+  }
+}
+function enterCard(event) {
+  document.getElementById("enterPaypallogin").style.display = "none";
+  document.getElementById("enterKlarnalogin").style.display = "none";
+  if (document.getElementById("creditcard").checked) {
+    document.getElementById("enterCCinfo").style.display = "block";
+  } else {
+    document.getElementById("enterCCinfo").style.display = "none";
+  }
+}
+function enterKlarnalogin(event) {
+  document.getElementById("enterCCinfo").style.display = "none";
+  document.getElementById("enterPaypallogin").style.display = "none";
+  if (document.getElementById("klarna").checked) {
+    document.getElementById("enterKlarnalogin").style.display = "block";
+  } else {
+    document.getElementById("enterKlarnalogin").style.display = "none";
+  }
+}
+function enterPaypallogin(event) {
+  document.getElementById("enterCCinfo").style.display = "none";
+  document.getElementById("enterKlarnalogin").style.display = "none";
+  if (document.getElementById("paypal").checked) {
+    document.getElementById("enterPaypallogin").style.display = "block";
+  } else {
+    document.getElementById("enterPaypallogin").style.display = "none";
+  }
+}
